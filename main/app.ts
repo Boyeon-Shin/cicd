5b445fc16c52213ae114287ea0044f77cdaf7991
Boyeon-Shin <anna007096@gmail.com>
2024-09-01 15:35:20 +0900

    .

--- main/app.ts
+++ main/app.ts
@@ -2,6 +2,10 @@ const server = Bun.serve({
   port: 6000,
   async fetch(req) {
     const path = new URL(req.url).pathname;
+    const file = Bun.file('./name.txt')
+    const message = await file.text()
+
+    console.log(message)
 
     if (path === "/") {
       return new Response("Welcome to Bun!");
